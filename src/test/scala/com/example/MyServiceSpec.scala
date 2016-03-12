4142b11430e705e0098e19783b9e234fa3eee0ad
boudes <boudes@lipn.univ-paris13.fr>
2016-03-12 11:38:24 +0100

    more tests

--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -7,12 +7,18 @@ import StatusCodes._
 
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
-  
+
   "MyService" should {
 
     "return a greeting for GET requests to the root path" in {
       Get() ~> myRoute ~> check {
         responseAs[String] must contain("Say hello")
+      }
+    }
+
+    "return a with love for GET requests to hello" in {
+      Get("/hello") ~> myRoute ~> check {
+        responseAs[String] must contain("Love")
       }
     }
 
