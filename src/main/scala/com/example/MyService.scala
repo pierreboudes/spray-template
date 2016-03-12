7b2a0c8af685c796275c8e20dbf00847c5c52311
boudes <boudes@lipn.univ-paris13.fr>
2016-03-12 11:40:02 +0100

    api with jsonp (simple example)

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -5,6 +5,20 @@ import spray.routing._
 import spray.http._
 import MediaTypes._
 
+// import spray.httpx.marshalling._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+import spray.httpx.SprayJsonSupport
+
+case class Reponse(entiers: List[Int], graine: Int, mot: String)
+object JsonReponse extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit val json = jsonFormat3(Reponse)
+}
+
+import JsonReponse._
+
+
+
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
 class MyServiceActor extends Actor with MyService {
@@ -18,7 +32,6 @@ class MyServiceActor extends Actor with MyService {
   // or timeout handling
   def receive = runRoute(myRoute)
 }
-
 
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
@@ -36,5 +49,35 @@ trait MyService extends HttpService {
           }
         }
       }
+    } ~
+  path("hello") {
+    get {
+      respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Love this!</h1>
+              </body>
+            </html>
+          }
+        }
     }
+  } ~
+  pathPrefix("api") {
+    jsonpWithParameter("callback") {
+      path("numbers") {
+        get {
+          parameter("seed".as[Int]) { seed =>
+            detach () {
+              validate(seed >= 0, "query parameter 'seed' must be >= 0") {
+                complete {
+                  Reponse((1 to seed).toList, seed, "hello")
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
